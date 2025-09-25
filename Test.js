514d02960d9f42f3e7febc0d56b3525dcb2d11cf
Musa Muhammad <musaawwaletudaye@gmail.com>
2025-09-25 23:33:01 +0100

    Added test,js

--- Test.js
+++ Test.js
@@ -1,1 +1,16 @@
-console.log ("Hello Javascript")
+console.log("Hello Javascript")
+
+for (let i = 1; i < 11; i++) {
+  console.log("Iteration " + i);
+}
+
+//  Iteration 1
+// Iteration 2
+// Iteration 3
+//  Iteration 4
+//  Iteration 5
+//  Iteration 6
+// Iteration 7
+// Iteration 8
+//  Iteration 9
+//  Iteration 10
